5e86919e37006078a4ebd881ec245702f83e128d
menno120 <31704017+menno120@users.noreply.github.com>
2019-03-01 23:25:58 +0100

    Added IKey interface and Key class

---
+++ src/classes/objects/Key.ts
@@ -0,0 +1,18 @@
+import IKey from "../../interfaces/IKey";
+import { SelectFunction } from "../QueryBuilder";
+import IReference from "../../interfaces/IReference";
+import Reference from "./Reference";
+
+class Key implements IKey {
+	key: IReference;
+	as?: string;
+	func?: SelectFunction;
+
+	constructor(key: IReference, as: string = null, func: SelectFunction = null) {
+		this.key = key;
+		this.as = as;
+		this.func = func;
+	}
+}
+
+export default Key;
---
+++ src/interfaces/IKey.ts
@@ -0,0 +1,10 @@
+import IReference from "./IReference";
+import { SelectFunction } from "../classes/QueryBuilder";
+
+interface IKey {
+	key: IReference;
+	as?: string;
+	func?: SelectFunction;
+}
+
+export default IKey;
