6ba58900f9ad8c4d4bf873688c43685357c067f2
menno120 <31704017+menno120@users.noreply.github.com>
2019-03-04 18:14:21 +0100

    added file that was not pused with last commit

--- tests/Key.spec.ts
+++ tests/Key.spec.ts
@@ -1,31 +1,33 @@
-import "mocha";
-import { expect } from "chai";
-import Key from "../src/classes/objects/Key";
-import Reference from "../src/classes/objects/Reference";
-import { SelectFunction } from "../src/classes/QueryBuilder";
+import 'mocha';
+import { expect } from 'chai';
+import Key from '../src/classes/objects/Key';
+import Reference from '../src/classes/objects/Reference';
+import { SelectFunction } from '../src/classes/QueryBuilder';
 
-describe("Key", () => {
-	describe("constructor", () => {
-		let key = new Key(new Reference("tablename", "id"), "outputValue");
+describe('Key', () => {
+	describe('constructor', () => {
+		let key = new Key(new Reference('tablename', 'id'), 'outputValue');
 
-		expect(key.key).to.equal({ table: "tablename", key: "id" });
-		expect(key.as).to.equal("outputValue");
+		// AssertionError: expected { table: 'tablename', key: 'id' } to equal { table: 'tablename', key: 'id' }
+
+		expect(key.key).to.equal({ table: 'tablename', key: 'id' });
+		expect(key.as).to.equal('outputValue');
 		expect(key.func).to.equal(null);
 	});
 
-	describe("constructor", () => {
-		let key = new Key(new Reference("tablename", "id"), null, SelectFunction.AVG);
+	describe('constructor', () => {
+		let key = new Key(new Reference('tablename', 'id'), null, SelectFunction.AVG);
 
-		expect(key.key).to.equal({ table: "tablename", key: "id" });
+		expect(key.key).to.equal({ table: 'tablename', key: 'id' });
 		expect(key.as).to.equal(null);
 		expect(key.func).to.equal(SelectFunction.AVG);
 	});
 
-	describe("constructor", () => {
-		let key = new Key(new Reference("tablename", "id"), "outputValue", SelectFunction.FULLTEXT);
+	describe('constructor', () => {
+		let key = new Key(new Reference('tablename', 'id'), 'outputValue', SelectFunction.FULLTEXT);
 
-		expect(key.key).to.equal({ table: "tablename", key: "id" });
-		expect(key.as).to.equal("outputValue");
+		expect(key.key).to.equal({ table: 'tablename', key: 'id' });
+		expect(key.as).to.equal('outputValue');
 		expect(key.func).to.equal(SelectFunction.FULLTEXT);
 	});
 });
